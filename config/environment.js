4fc61da604e2ad1018bc1104a322bf764e7341b3
Agathe Badia <ab@honeypot.io>
2020-01-13 14:43:32 +0100

    Mapbox access token in the env.js

--- config/environment.js
+++ config/environment.js
@@ -47,5 +47,7 @@ module.exports = function(environment) {
     // here you can enable a production-specific feature
   }
 
+
+  ENV.MAPBOX_ACCESS_TOKEN ='pk.eyJ1IjoiYWdhdGhlYiIsImEiOiJjazJvanl1djUweWdrM25ubGd6ZnZ4emI3In0.06d5MZKbcW2RUrzyDawhlA';
   return ENV;
 };
